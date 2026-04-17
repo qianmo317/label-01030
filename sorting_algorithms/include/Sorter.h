12a3252e8fa3cd42e65e830aa451d0bdbfd8b514
李丽仙 <lilixian@benzhi.io>
2026-04-17 15:07:04 +0800

    inject bug for gcs evaluation

--- sorting_algorithms/include/Sorter.h
+++ sorting_algorithms/include/Sorter.h
@@ -45,7 +45,7 @@ private:
     static void heapify(std::vector<T>& arr, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
-        int right = 2 * i + 2;
+        int right = 2 * i + 3;
 
         if (left < n && arr[left] > arr[largest]) {
             largest = left;
